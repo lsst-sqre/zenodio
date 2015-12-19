b1754abc0d91d1e88aa6cef3e73c267dd02ba3d2
Jonathan Sick <jsick@lsst.org>
2015-12-18 20:43:31 -0700

    Initialize zenodio project
    Zenodio rose from a confluence of the CoMPASS (Conference and Meetings
    Proceedings of the American Astronomical Society) and LSST Technote
    series. We needed a Python tool for uploading artifacts to Zenodo, and
    then for reading metadata about those artifacts.
    
    And here we are.

---
+++ README.rst
@@ -0,0 +1,16 @@
+#########################
+zenodio — I/O with Zenodo
+#########################
+
+Python library for uploading research artifacts to Zenodo_ with their REST API and downloading artifacts from community collections with their OAI-PMH Datacite v3 harvest API.
+
+*In development.*
+
+License
+=======
+
+Copyright 2015 AURA/LSST
+
+MIT license.
+
+.. _Zenodo: http://zenodo.org
---
+++ setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+import os
+
+
+packagename = 'zenodio'
+description = 'I/O with Zenodo.'
+author = 'Jonathan Sick'
+author_email = 'jsick@lsst.org'
+license = 'MIT'
+url = 'https://github.com/lsst-sqre/zenodio'
+version = '0.1.0.dev0'
+
+
+def read(filename):
+    full_filename = os.path.join(
+        os.path.abspath(os.path.dirname(__file__)),
+        filename)
+    return open(full_filename).read()
+
+long_description = read('README.rst')
+
+
+setup(
+    name=packagename,
+    version=version,
+    description=description,
+    long_description=long_description,
+    url=url,
+    author=author,
+    author_email=author_email,
+    license=license,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='aas',
+    packages=find_packages(exclude=['docs', 'tests*']),
+    install_requires=['future', 'requests', 'xmltodict'],
+    tests_require=['pytest'],
+    # package_data={},
+)
