cf1758e3ec97ef0a286c6c085ef85c2110106afe
Jonathan Sick <jsick@lsst.org>
2015-12-19 12:21:41 -0700

    Test zenodo_harvest_url
    Ensure that oai_datacite3 is the default.

--- tests/test_harvest.py
+++ tests/test_harvest.py
@@ -1,7 +1,7 @@
 import pkg_resources
 import pytest
 
-from zenodio.harvest import Datacite3Collection
+from zenodio.harvest import Datacite3Collection, zenodo_harvest_url
 
 
 @pytest.fixture
@@ -16,3 +16,11 @@ def test_read_lisa7(lisa7_posters_xml):
     collection = Datacite3Collection.from_collection_xml(lisa7_posters_xml)
     records = [r for r in collection.records()]
     assert len(records) > 0
+
+
+def test_zenodo_harvest_url():
+    community_id = 'lsst-dm'
+    url = zenodo_harvest_url(community_id)
+    expected = 'http://zenodo.org/oai2d?verb=ListRecords&' \
+               'metadataPrefix=oai_datacite3&set=user-lsst-dm'
+    assert url == expected
