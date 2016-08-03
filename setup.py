94d1c3f3b10f3e58ca6fe25b36407e9c97557222
Michael Currie <mcurrie@bruceforceresearch.com>
2016-08-03 15:41:03 +0700

    Increment version to 0.2.1.dev0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ author = 'Jonathan Sick'
 author_email = 'jsick@lsst.org'
 license = 'MIT'
 url = 'https://github.com/lsst-sqre/zenodio'
-version = '0.1.1.dev0'
+version = '0.2.1.dev0'
 
 
 def read(filename):
